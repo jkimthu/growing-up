519ae059bdd77eaafd16ee2b24da6b98fb3c3c78
jen nguyen <j.kimthu.nguyen@gmail.com>
2016-02-03 13:22:38 +0100

    Finish part one: time, length, and mu concatenation

--- nSync.m
+++ nSync.m
@@ -22,23 +22,24 @@
 
 % Envisioned data matrix:
 
-%        row     Time     Lngth     drop?      curve#     cc stage
-%         1        t        x        nan         1           1
-%         2        t        x         0          1           2
-%         3        t        x         0          1           3
-%         4        t        x         1          2           1
-%         5        t        x         0          2           2
-%         6        t        x         0          2           3
-%         7        t        x         1          3           1
-%         8        t        x         0          3           2
-%         9        t        x         0          3           3
-%         10       t        x         1          4           1
+%        row     Time     Lngth     Mu      drop?      curve#     cc stage
+%         1        t        x        u       nan         1           1
+%         2        t        x        u        0          1           2
+%         3        t        x        u        0          1           3
+%         4        t        x        u        1          2           1
+%         5        t        x        u        0          2           2
+%         6        t        x        u        0          2           3
+%         7        t        x        u        1          3           1
+%         8        t        x        u        0          3           2
+%         9        t        x        u        0          3           3
+%         10       t        x        u        1          4           1
 
 
 %       where,
 %                row     =  row number, obvi
 %                t       =  all timepoints associated with concatinated length trajectories
 %                x       =  length values from concatentated length trajectories
+%                mu      =  calculated growth rates from SlidingFits.m
 %                drop?   =  finding where individual cell cycles start and end, a boolean 
 %                curve   =  an id number for each individual cell cycle
 %                stage   =  time since birth / duration of entire cycle
@@ -54,6 +55,7 @@
 
 % OK! Lez go!
 
+
 %%
 %   Initialize.
 
@@ -61,6 +63,47 @@ load('2015-08-10-Mu-length.mat');
 D7 = D6;
 M7 = M6;
 
+clear D6 M6 Mu_stats;
 
 %%
+%
+%   Part One.
+%   Concatenating measured length values with associated time coordinates
+%
+%
+
+% Initialize data vectors for concatenation
+
+Time = [];
+lengthVals = [];
+muVals = [];
+
+
+% Select xy positions for analysis / concatenation
+
+for n=1:2
+                                                                           
+    for m = 1:length(M7{n})                                                % use length of growth rate data as it is 
+                                                                           % slightly truncated from full length track due
+        trackDuration = length(M7{n}(m).Parameters(:,1));                  % to sliding fit
+                                                                           
+        timeTrack = T(3:trackDuration+2,n)/(60*60);                        % collect timestamp (hr)
+        Time = [Time; timeTrack];                                          % concenate timestamp
+       
+        lengthTrack = D7{n}(m).MajAx(3:trackDuration+2);                   % collect lengths (um)
+        lengthVals = [lengthVals; lengthTrack];                            % concatenate lengths
+        
+        muTrack = M7{n}(m).Parameters(:,1);                                % collect elongation rates (1/hr)
+        muVals = [muVals; muTrack;                                         % concatenate growth rates
+        
+    end
+end
+muVals(muVals<0) = NaN;
+
+%Tbin = ceil(Time*20);                                                     % time increments = 0.005 hr      
+%Tbinned = accumarray(Tbin,lengthVals,[],@(x) {x});
+%clear Num_mu Ttrack ThreeMins m n;
+
+
+
 
