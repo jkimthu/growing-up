8d7580c9b7efbec7ef3b3d4ee3b15643b76dfcc5
jen nguyen <j.kimthu.nguyen@gmail.com>
2016-02-04 15:41:58 +0100

    Add track# and drop boolean to data matrix

--- nSync.m
+++ nSync.m
@@ -22,17 +22,17 @@
 
 % Envisioned data matrix:
 
-%        row     Time     Lngth     Mu      drop?      curve#     cc stage
-%         1        t        x        u       nan         1           1
-%         2        t        x        u        0          1           2
-%         3        t        x        u        0          1           3
-%         4        t        x        u        1          2           1
-%         5        t        x        u        0          2           2
-%         6        t        x        u        0          2           3
-%         7        t        x        u        1          3           1
-%         8        t        x        u        0          3           2
-%         9        t        x        u        0          3           3
-%         10       t        x        u        1          4           1
+%        row     Track#   Time     Lngth     Mu      drop?      curve#     cc stage
+%         1        1        t        x        u       nan         1           1
+%         2        1        t        x        u        0          1           2
+%         3        1        t        x        u        0          1           3
+%         4        1        t        x        u        1          2           1
+%         5        1        t        x        u        0          2           2
+%         6        1        t        x        u        0          2           3
+%         7        1        t        x        u        1          3           1
+%         8        1        t        x        u        0          3           2
+%         9        1        t        x        u        0          3           3
+%         10       1        t        x        u        1          4           1
 
 
 %       where,
@@ -74,9 +74,17 @@ clear D6 M6 Mu_stats;
 
 % Initialize data vectors for concatenation
 
+trackNumber = [];
+trackCounter = 1;
+
 Time = [];
 lengthVals = [];
 muVals = [];
+
+isDrop = []; 
+dropThreshold = -0.75;                                                     % consider greater negatives a division event
+
+
 
 
 % Select xy positions for analysis / concatenation
@@ -85,25 +93,44 @@ for n=1:2
     
     for m = 1:length(M7{n})                                                % use length of growth rate data as it is
                                                                            % slightly truncated from full length track due
-        trackDuration = length(M7{n}(m).Parameters(:,1));                  % to sliding fit
+        %   track #                                                        % to sliding fit
+        trackDuration = length(M7{n}(m).Parameters(:,1));
+        Track = ones(trackDuration,1);
+        trackNumber = [trackNumber; trackCounter*Track];
+        trackCounter = trackCounter + 1;
         
+        %   time
         timeTrack = T(3:trackDuration+2,n)/(60*60);                        % collect timestamp (hr)
         Time = [Time; timeTrack];                                          % concenate timestamp
         
+        %   lengths
         lengthTrack = D7{n}(m).MajAx(3:trackDuration+2);                   % collect lengths (um)
         lengthVals = [lengthVals; lengthTrack];                            % concatenate lengths
         
+        %   growth rate
         muTrack = M7{n}(m).Parameters(:,1);                                % collect elongation rates (1/hr)
         muVals = [muVals; muTrack];                                        % concatenate growth rates
+        
+        %   drop?
+        dropTrack = diff(lengthTrack);
+        toBool = dropTrack < dropThreshold;                                % converts different to a Boolean based on dropThreshold
+        toBool = [0; toBool];                                              % add zero to front, to even track lengths
+        isDrop = [isDrop; toBool];
         
     end
 end
 muVals(muVals<0) = NaN;
 
-%Tbin = ceil(Time*20);                                                     % time increments = 0.005 hr
-%Tbinned = accumarray(Tbin,lengthVals,[],@(x) {x});
-%clear Num_mu Ttrack ThreeMins m n;
+
+% Compile data into single matrix
+dataMatrix = [trackNumber Time lengthVals muVals isDrop];                  
+
 
 
 %%
-        
+%
+%   Part Two.
+%   Identify birth/division events within individual trajectories
+%
+%
+
