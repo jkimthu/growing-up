40717f0ec5b81388a44df676900a9bc8edcee102
jen nguyen <j.kimthu.nguyen@gmail.com>
2016-02-05 14:28:56 +0100

    Add curve finder to id full curves within distinct tracks

--- nSync.m
+++ nSync.m
@@ -2,8 +2,7 @@
 
 
 %  Goal: Searching for synchrony in growth data.
-%
-%  Last edit: Jen Nguyen, February 3rd 2016
+%  Last edit: Jen Nguyen, February 5rd 2016
 
 
 % Let's first take one experiment, say 60 min fluctuations, and see if
@@ -22,17 +21,17 @@
 
 % Envisioned data matrix:
 
-%        row     Track#   Time     Lngth     Mu      drop?      curve#     cc stage
-%         1        1        t        x        u       nan         1           1
-%         2        1        t        x        u        0          1           2
-%         3        1        t        x        u        0          1           3
-%         4        1        t        x        u        1          2           1
-%         5        1        t        x        u        0          2           2
-%         6        1        t        x        u        0          2           3
-%         7        1        t        x        u        1          3           1
-%         8        1        t        x        u        0          3           2
-%         9        1        t        x        u        0          3           3
-%         10       1        t        x        u        1          4           1
+%        row     Track#    Time     Lngth     Mu      drop?      curve#     cc stage
+%         1        1         t        x        u        0*         1           1
+%         2        1         t        x        u        0          1           2
+%         3        1         t        x        u        0          1           3
+%         4        1         t        x        u        1          2           1
+%         5        1         t        x        u        0          2           2
+%         6        1         t        x        u        0          2           3
+%         7        1         t        x        u        1          3           1
+%         8        1         t        x        u        0          3           2
+%         9        1         t        x        u        0          3           3
+%         10       1         t        x        u        1          4           1
 
 
 %       where,
@@ -68,14 +67,14 @@ clear D6 M6 Mu_stats;
 %%
 %
 %   Part One.
-%   Concatenating measured length values with associated time coordinates
+%   Assemble the ultimate data matrix!
 %
-%
+
 
 % Initialize data vectors for concatenation
 
 trackNumber = [];
-trackCounter = 1;
+trackCounter = 1;                                                          
 
 Time = [];
 lengthVals = [];
@@ -84,6 +83,8 @@ muVals = [];
 isDrop = []; 
 dropThreshold = -0.75;                                                     % consider greater negatives a division event
 
+curveFinder = [];
+%curveCounter = 0;                                                          
 
 
 
@@ -97,7 +98,7 @@ for n=1:2
         trackDuration = length(M7{n}(m).Parameters(:,1));
         Track = ones(trackDuration,1);
         trackNumber = [trackNumber; trackCounter*Track];
-        trackCounter = trackCounter + 1;
+        trackCounter = trackCounter + 1;                                   % cumulative count of tracks in condition
         
         %   time
         timeTrack = T(3:trackDuration+2,n)/(60*60);                        % collect timestamp (hr)
@@ -114,16 +115,55 @@ for n=1:2
         %   drop?
         dropTrack = diff(lengthTrack);
         toBool = dropTrack < dropThreshold;                                % converts different to a Boolean based on dropThreshold
-        toBool = [0; toBool];                                              % add zero to front, to even track lengths
+        toBool = [0; toBool];                                              % * add zero to front, to even track lengths
         isDrop = [isDrop; toBool];
+        
+        %   curve finder                                                   % finds and labels full curves within a single track
+        fullCurves = sum(toBool) - 1;                                      % hint: full curves are bounded by ones
+        curveTrack = zeros(length(toBool),1);
+        curveCounter = 0;                                                  % 1. disregard incomplete first curve
+                                                                           %    by starting count at 0
+        for i = 1:length(toBool)
+            
+            if toBool(i) == 0
+                curveTrack(i,1) = curveCounter;                            
+                
+            elseif (toBool(i) == 1)
+                curveCounter = curveCounter + 1;
+                
+                if curveCounter <= fullCurves
+                    curveTrack(i,1) = curveCounter;
+                else                                                       % 2. how to disregard final incomplete segment? 
+                    break                                                  %    stop when curveCount exceeds number of fullCurves
+                end
+            end
+        end
+        
+        curveFinder = [curveFinder; curveTrack];                           % all incomplete curves are filled with 0
+
+        
         
     end
 end
 muVals(muVals<0) = NaN;
 
 
+%%
+
+% drafting curveFinder, to count the number of full curves per track
+
+%        1.   
+
+%        3.   
+%        4.   fill with zeros for rest of track duration
+
+
+
+%%
+
+
 % Compile data into single matrix
-dataMatrix = [trackNumber Time lengthVals muVals isDrop];                  
+dataMatrix = [trackNumber Time lengthVals muVals isDrop curveFinder];                  
 
 
 
