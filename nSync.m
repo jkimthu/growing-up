0043b28852b705578001807ed93f4933c97a88dd
jen nguyen <j.kimthu.nguyen@gmail.com>
2016-02-11 13:18:57 +0100

    Load dataMatrices such that constant envir is always first

--- nSync.m
+++ nSync.m
@@ -2,24 +2,28 @@
 
 
 %  Goal: Searching for synchrony in growth data.
-%  Last edit: Jen Nguyen, February 5rd 2016
+%        This script plots up multiple views of cell cycle stage.
+%
+%  Last edit: Jen Nguyen, February 11th 2016
 
 
-% Let's first take one experiment, say 60 min fluctuations, and see if
-% segregating sub-populations by growth phase leads to clear behavioral
-% differences, or at least less variation within groups.
 
 
-% Let's define growth phase as a specific fraction of the growth curve.
+% Growth phase is defined as a specific fraction of the growth curve, as
+% calculated and assembled in matrixBuilder.
+%
+% The intended input for these scripts is the following data matrix,
+% saved with the naming convention of:
 
-%       1. Determine duration of each individual growth curve
-%               a. How does the mean and stdev of this vary between expts?
-%       2. Associate each time point (in growth curve) with a fraction of cell cycle
-%       3. Bin data belonging to a desired window.
-%       4. Plot the bejeezy out of these cell cycle based groupings!
+% dmMMDD-cond.mat
 
+%      where,
+%              dm  =  dataMatrix                  (see matrixBuilder.m)
+%              MM  =  month of experimental date
+%              DD  =  day of experimental date
+%       condition  =  experimental condition      (fluc or const)
+%
 
-% Envisioned data matrix:
 
 %        row      Track#    Time     Lngth      Mu       drop?      curve#    timeSinceBirth    curveDuration    cc stage
 %         1         1         t        x         u         0*         1              0                3              1
@@ -53,6 +57,7 @@
 %               ccStage = time since birth / total curve duration
 
 
+
 % Considerations:
 %
 %       1. Does separation between phase-sorted subpopulations occur?
@@ -66,158 +71,34 @@
 %%
 %   Initialize.
 
-load('2015-08-10-Mu-length.mat');
-D7 = D6;
-M7 = M6;
+dmDirectory = dir('dm*.mat');
+names = {dmDirectory.name}; % loaded alphabetically
 
-clear D6 M6 Mu_stats;
+for dm = 1:length(names)
+    load(names{dm});                
+    dataMatrices{dm} = dataMatrix;                                         % 1 = const; 2 = fluc
+end
+
+clear dataMatrix dmDirectory dm;
+clear names;
+
 
 %%
+
+% Plotz
+
+% 1. Cell cycle fraction over time
 %
-%   Part One.
-%   Assemble the ultimate data matrix!
+%           i. line (average)
+%          ii. scatter
+%         iii. heatmap
+%          
+
+
+% 2. Cycle durations over time 
 %
-
-
-% Initialize data vectors for concatenation
-
-trackNumber = [];
-trackCounter = 1;                                                          
-
-Time = [];
-lengthVals = [];
-muVals = [];
-
-isDrop = []; 
-dropThreshold = -0.75;                                                     % consider greater negatives a division event
-
-curveFinder = [];                                                        
-
-allDurations = [];
-curveDurations = [];
-
-timeSinceBirth = [];
-
-% Select xy positions for analysis / concatenation
-
-for n=1:10 
-     
-    for m = 1:length(M7{n})                                                % use length of growth rate data as it is
-                                                                           % slightly truncated from full length track due
-                                                                           % to sliding fit
-                                                                           
-        %   track #                                                        
-        trackDuration = length(M7{n}(m).Parameters(:,1));
-        Track = ones(trackDuration,1);
-        trackNumber = [trackNumber; trackCounter*Track];
-        trackCounter = trackCounter + 1;                                   % cumulative count of tracks in condition
-        
-        
-        
-        %   time
-        timeTrack = T(3:trackDuration+2,n)/(60*60);                        % collect timestamp (hr)
-        Time = [Time; timeTrack];                                          % concenate timestamp
-        
-        
-        
-        %   lengths
-        lengthTrack = D7{n}(m).MajAx(3:trackDuration+2);                   % collect lengths (um)
-        lengthVals = [lengthVals; lengthTrack];                            % concatenate lengths
-        
-        
-        
-        %   growth rate
-        muTrack = M7{n}(m).Parameters(:,1);                                % collect elongation rates (1/hr)
-        muVals = [muVals; muTrack];                                        % concatenate growth rates
-        
-        
-        
-        %   drop?
-        dropTrack = diff(lengthTrack);
-        toBool = dropTrack < dropThreshold;                                % converts different to a Boolean based on dropThreshold
-        toBool = [0; toBool];                                              % * add zero to front, to even track lengths
-        isDrop = [isDrop; toBool];
-        
-        
-        
-        %   curve finder                                                   
-        fullCurves = sum(toBool) - 1;                                      
-        curveTrack = zeros(length(toBool),1);
-        curveCounter = 0;                                                  % finds and labels full curves within a single track
-                                                                           % hint: full curves are bounded by ones
-        for i = 1:length(toBool) 
-            if toBool(i) == 0                                              % 1. disregard incomplete first curve
-                curveTrack(i,1) = curveCounter;                            %    by starting count at 0   
-            elseif (toBool(i) == 1)
-                curveCounter = curveCounter + 1;                           % 2. how to disregard final incomplete segment? 
-                if curveCounter <= fullCurves                              %    stop when curveCount exceeds number of fullCurves
-                    curveTrack(i,1) = curveCounter;
-                else                                                       % all incomplete curves are filled with 0
-                    break                                                  
-                end
-            end
-        end
-        curveFinder = [curveFinder; curveTrack];
-        
-        
-        
-        %   time since birth  
-        isolateEvents = timeTrack.*toBool;
-        eventTimes = isolateEvents(isolateEvents~=0);                      % 1. find time of division/birth events
-        
-        tsbPerTrack = zeros(trackDuration,1);                              % 2. calculate: 
-        durationsPerTrack = zeros(fullCurves,1);                           %     - time since birth for each timestep
-                                                                           %     - final timestep is also cycle duration
-        for currentCurve = 1:fullCurves; % per individual curve
-            
-            currentBirthRow = find(timeTrack == eventTimes(currentCurve)); % 3. store individual curve durations in single vector
-            nextBirthRow = find(timeTrack == eventTimes(currentCurve+1));  
-            currentTimes = timeTrack(currentBirthRow:nextBirthRow-1);      % 4. cell cycle fraction = time since birth / total curve duration
-            
-            tsbPerCurve = currentTimes - timeTrack(currentBirthRow);
-            tsbPerTrack(currentBirthRow:nextBirthRow-1,1) = tsbPerCurve;
-            
-            durationsPerTrack(currentCurve) = tsbPerCurve(end); % for cell cycle durations
-        end
-        
-        timeSinceBirth = [timeSinceBirth; tsbPerTrack]; % compile per condition
-        allDurations = [allDurations; durationsPerTrack]; % compiled durations
-        
-        
-        
-        %   cycle duration
-        trackBuilder = zeros(trackDuration,1);
-        for j = 1:length(curveTrack)
-            if curveTrack(j) == 0
-                continue
-            else
-                trackBuilder(j,1) = durationsPerTrack(curveTrack(j)); % for current track
-            end
-        end
-        curveDurations = [curveDurations; trackBuilder]; % collect all durations for analytical ease (ccStage)
-        
-        
-        
-        %   fraction of cell cycle
-        ccFraction = timeSinceBirth./curveDurations;                       % NaN =  no full cycle
-                                                                           % 0   =  start of full cycle
-                                                                           % 1   =  end of full cycle
-    
-    end % for m
-end % for n
-%muVals(muVals<0) = NaN;
-
-
-%%
-
-% Compile data into single matrix
-dataMatrix = [trackNumber Time lengthVals muVals isDrop curveFinder timeSinceBirth curveDurations ccFraction];
-
-
-
-%%
-clear currentCurve currentBirthRow nextBirthRow currentTimes tsbPerCurve
-clear timeSinceBirth
+%
+%
 
 
 
