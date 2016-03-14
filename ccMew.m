ee507e2ee1a966d7503786b1ef0c971c139e3be4
Jen Nguyen <jknguyen@mit.edu>
2016-03-14 12:43:58 +0100

    Reformat cell cycle fraction vs growth rate plot

--- ccMew.m
+++ ccMew.m
@@ -92,9 +92,10 @@ for condition = 1:2          % 1 = constant, 2 = fluctuating
     errorStage = stdStage./sqrt(nStage);
     
     
-    % 5. plot
+    % 5. plot mean and std
     if condition == 1
         plot(meanStage,'k')
+        axis([0,21,-.2,1.2])
         hold on
         errorbar(meanStage,stdStage,'k')
     else
@@ -104,61 +105,5 @@ for condition = 1:2          % 1 = constant, 2 = fluctuating
     end
     
 end
-%%
 
-
-%
-for condition = 1:2;                                  % for looping between conditions
-    
-    interestingData = dataMatrices{condition};      % condition: 1 = constant, 2 = fluctuating
-    currentTimepoint = firstTimepoint;              % initialize first timepoint as current timepoint
-    
-    % 1. isolate time and ccStage data
-    timeStamps = interestingData(:,2);
-    ccStage = interestingData(:,9);
-    
-    % 2. accumulate data by associated time bin
-    timeBins = ceil(timeStamps*binsPerHour);
-    binnedByTime = accumarray(timeBins,ccStage,[],@(x) {x});
-    
-    for period = 1:numPeriods
-        
-        % 3. establish current period in loop
-        currentPeriod = currentTimepoint:(currentTimepoint + binsPerPeriod -1);
-        if period < numPeriods
-            currentStages = binnedByTime(currentPeriod);
-        else
-            currentStages = binnedByTime(currentTimepoint:end);
-        end
-        currentTimepoint = currentTimepoint + binsPerPeriod; % re-define currentTimepoint for next loop cycle
-        
-        % 4. calculate mean cell cycle stage per time bin within current period
-        meanStage = cell2mat( cellfun(@nanmean,currentStages,'UniformOutput',false) );
-        stdStage = cell2mat( cellfun(@nanstd,currentStages,'UniformOutput',false) );
-        nStage = cell2mat( cellfun(@length,currentStages,'UniformOutput',false) );
-        errorStage = stdStage./sqrt(nStage);
-        
-        % 5. plot mean for current period
-        %    ...but before plotting, remove nans from cell cycle and time data
-        eventMask = find(~isnan(meanStage));                                       % find indices of cell cycle data
-        meanStage = meanStage(eventMask);                                          % trim all nans from ccStage vector
-        currentTime = periodTime;
-        currentTime = currentTime(eventMask);                                          % trim all nans from time vector
-        currentFraction = currentTime./periodDuration;
-        
-        figure(1)
-        if condition == 1
-            plot(currentFraction,meanStage,'color',[0,0,0]+(1/period)*[1,1,1],'linewidth',1.01)   % constant
-            axis([0,1,0,1])
-            grid on
-            hold on
-        else
-            plot(currentFraction,meanStage,'color',[0.1,0,1]+(1/period)*[0.2,0.6,0],'linewidth',1.01)   % fluctuating (blue)
-            hold on
-        end
-        
-    end
-    clear period
-
-end
 
