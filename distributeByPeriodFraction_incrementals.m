1d92d489a5d241ec2fb7b5473bba2c6b3ea8cd5f
Jen Nguyen <jknguyen@mit.edu>
2016-08-13 15:51:51 +0200

    plot distributions of instantaneous mass added, comparing low and high of fluc

--- distributeByPeriodFraction_incrementals.m
+++ distributeByPeriodFraction_incrementals.m
@@ -1,8 +1,8 @@
-% timestamp-dependent distributions
+% timestamp-dependent distributions of dataMatrix parameters
 
 
 
-%  Goal: plot distributions of cell cycle duration and added mass,
+%  Goal: plot distributions of instantaneous added mass,
 %        based on period fraction
 
 %  Last edit: Jen Nguyen, August 13th 2016
@@ -26,40 +26,33 @@
 % The intended input for this script is the following data matrix,
 % saved with the naming convention of:
 
-% dFMMDD.mat
-% dCMMDD.mat
+% dmMMDD-const.mat
+% dmMMDD-fluc.mat
 
 % Initialize data.
-load('dC0810_mit.mat');
-load('dF0810_mit.mat');
-dC = dC0810_mit;
-dF = dF0810_mit;
-clear dC0810_mit dF0810_mit;
+load('dm0810-const.mat');
+dC = dataMatrix;
+
+load('dm0810-fluct.mat');
+dF = dataMatrix;
+clear dataMatrix;
 
 % OK! Lez go!
 
-%%
 
-
-
-% 0. initiaize duration data
-duration_c = dC(:,1);
-duration_f = dF(:,1);
 
 % 0. initiaize mass data
-addedMass_c = dC(:,2);
-addedMass_f = dF(:,2);
+cumulativeMass_c = dC(:,3);
+cumulativeMass_f = dF(:,3);
 
 % 0. initiaize timestamp data
-timeStamp_c = dC(:,3);
-timeStamp_f = dF(:,3);
+time_c = dC(:,2);
+time_f = dF(:,2);
 
-dataz{1} = duration_c;
-dataz{2} = duration_f;
-dataz{3} = addedMass_c;
-dataz{4} = addedMass_f;
-dataz{5} = timeStamp_c;
-dataz{6} = timeStamp_f;
+dataz{1} = cumulativeMass_c;
+dataz{2} = cumulativeMass_f;
+dataz{3} = time_c;
+dataz{4} = time_f;
 
 for i = 1:2 % Remove zeros
         currentVar = dataz{i};
@@ -69,11 +62,9 @@ for i = 1:2 % Remove zeros
         dataz_trimmed{i} = currentVar;
         
         if i == 1
-            dataz_trimmed{3} = addedMass_c(nanFilter);
-            dataz_trimmed{5} = timeStamp_c(nanFilter);
+            dataz_trimmed{3} = time_c(nanFilter);
         else
-            dataz_trimmed{4} = addedMass_f(nanFilter);
-            dataz_trimmed{6} = timeStamp_f(nanFilter);    
+            dataz_trimmed{4} = time_f(nanFilter);    
         end
 end
 clear duration_c duration_f addedMass_c addedMass_f timeStamp_c timeStamp_f currentVar i nanFilter;
@@ -94,21 +85,36 @@ firstHour = 5;                                  % time at which to initate analy
 finalHour = 10;                                 % time at which to terminate analysis
 
 % 1. isolate data of interest based on condition
-timeStamps = dataz_trimmed{6}; %fluc
-addedMass = dataz_trimmed{4};
-cycleDuration = dataz_trimmed{2};
+timeStamps = dataz_trimmed{4}; %fluc
+instantaneousMass_f = diff(dataz_trimmed{2});
+instantaneousMass_c = diff(dataz_trimmed{1});
+
 
 % trim off lower timepoints
 interestingPoints = find(timeStamps >= firstHour);
 interestingTimes = timeStamps(interestingPoints);
-interestingMass = addedMass(interestingPoints);
-interestingDurations = cycleDuration(interestingPoints);
+interestingMass = instantaneousMass_f(interestingPoints);
 
 % trim off higher timepoints
 interestingPoints_2 = find(interestingTimes <= finalHour);
-interestingTimes_trimmed = interestingTimes(interestingPoints_2);
-interestingMass_trimmed = interestingMass(interestingPoints_2);
-interestingDurations_trimmed = interestingDurations(interestingPoints_2);
+interestingTimes_2 = interestingTimes(interestingPoints_2);
+interestingMass_2 = interestingMass(interestingPoints_2);
+
+% trim off negatives
+negFilter_f = find(interestingMass_2 >= 0);
+interestingTimes_3 = interestingTimes_2(negFilter_f);
+interestingMass_3 = interestingMass_2(negFilter_f);
+
+negFilter_c = find(instantaneousMass_c >= 0);
+instantaneousMass_c_3 = instantaneousMass_c(negFilter_c);
+
+% trim off crazy add-ons
+posFilter_f = find(interestingMass_3 <= .4);
+interestingTimes_trimmed = interestingTimes_3(posFilter_f);
+interestingMass_trimmed = interestingMass_3(posFilter_f);
+
+posFilter_c = find(instantaneousMass_c_3 <= .4);
+instantaneousMass_c_trimmed = instantaneousMass_c_3(posFilter_c);
 
 
 % 2. accumulate data by associated time & concentration bin
@@ -129,30 +135,21 @@ for i = 1:length(fractionBins)
 end
 clear i;
 
-binnedByConcentration_mass = accumarray(concentrationBins,interestingMass_trimmed,[],@(x) {x});
-binnedByConcentration_duration = accumarray(concentrationBins,interestingDurations_trimmed,[],@(x) {x});
 
-% Yeah!
+binnedByConcentration_mass = accumarray(concentrationBins,interestingMass_trimmed,[],@(x) {x});
+%binnedByConcentration_mass = accumarray(fractionBins,interestingMass_trimmed,[],@(x) {x});
+
+
+
 
 % 4. Plot distribution of added sizes per cell cycle
 figure(1)
-histogram(binnedByConcentration_mass{1},'BinWidth',0.1) %low
+histogram(binnedByConcentration_mass{1},'BinWidth',0.008) %low
 hold on
-histogram(binnedByConcentration_mass{2},'BinWidth',0.1) %high
-hold on
-histogram(dataz_trimmed{3},'BinWidth',0.1) %high
-xlabel('added size per cell cycle')
-legend('low','high','stable ave')
+histogram(binnedByConcentration_mass{2},'BinWidth',0.008) %high
+%hold on
+%histogram(instantaneousMass_c_trimmed,'BinWidth',0.01) %high
+xlabel('instantaneous added size')
+legend('low','high')
 
-
-
-% 6. Plot distribution of cell cycle durations
-figure(2)
-histogram(binnedByConcentration_duration{1},'BinWidth',0.1) %low
-hold on
-histogram(binnedByConcentration_duration{2},'BinWidth',0.1) %high
-hold on
-histogram(dataz_trimmed{1},'BinWidth',0.1) %high
-xlabel('cell cycle durations')
-legend('low','high','stable ave')
-
+% Yeah!
