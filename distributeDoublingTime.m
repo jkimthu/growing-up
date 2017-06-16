b3c2197cf893b79b9025a909523643c156c71846
Jen Nguyen <jknguyen@mit.edu>
2017-06-16 07:49:39 +0200

    plot PDFs of division time, accumulating data from 0.5 uM condition (ave) from all six 5min fluc expts

--- distributeDoublingTime.m
+++ distributeDoublingTime.m
@@ -1,115 +1,127 @@
-%% distributeDoublingTime
+%% distributeDivisionTime
 
 % Goal: this script plots the PDF of division times from a given condition
 
 
 % Strategy:
-%           0. initialize data matrix
-%           1. isolate condition of interest
-%                  2. isolate time period of interest
-%                  3. collect drop data and track ID
-%                  4. count number of drops per track
-%                         5. if at least two, there's a whole curve:
-%                                 i. align drop and time data
-%                                ii. measure time between birth and division
-%                               iii. save in div time array
-%                            else, move onto next track
-%                  6. build PDF from completed div time array
-%           7. repeat for any other conditions of interest
+%           0. for each experiment,
+%                 1. initialize data matrix
+%                 2. isolate condition of interest
+%                 3. isolate time period of interest
+%                 4. collect drop data and track ID
+%                 5. count number of drops per track
+%                       6. if at least two, there's a whole curve:
+%                               i. align drop and time data
+%                              ii. measure time between birth and division
+%                             iii. save in div time array
+%                          else, move onto next track
+%                 6. build PDF from completed div time array
+%             repeat for any other conditions of interest
 
 
 
-% last edit: jen, 2017 Jun 14
+% last edit: jen, 2017 Jun 16
 
 % OK LEZ GO!
 
 %%
-% 0. initialize data matrix
+% 0. initialize experiments for analysis
+
 clear
-experiments{1} = 'dm-t300-2017-01-06.mat';
-%cd('/Users/jen/Documents/StockerLab/Data/2017-01-06  (t300)');
-
-load('meta.mat');
-meta = meta_2017jan06;
-
-load(experiments{1});
+experiments{1} = '2017-01-06';
+experiments{2} = '2017-01-16';
+experiments{3} = '2017-01-17';
+experiments{4} = '2017-01-18';
+experiments{5} = '2017-02-10';
+experiments{6} = '2017-02-11';
 
 %%
-%clear dataMatrix;
-%load('dm-2017-05-26.mat');
-%conditions = [1,2,3,4,5,6];
-c = 3;
-
-
-
-% 1. isolate condition of interest
-trimmedData = dataMatrix(dataMatrix(:,28) == c,:);
-
-
-
-% 2. isolate time period of interest
-minTime = meta(c,3); % based on where growth rate seems to stabilize
-maxTime = meta(c,4);
-%%
-% remove data from timepoints earlier than minTime
-trimmedData2 = trimmedData(trimmedData(:,2) >= minTime,:);
-
-% remove data from timepoints later than maxTime
-trimmedData_final = trimmedData2(trimmedData2(:,2) <= maxTime,:);
-
-
-
-% 3. collect drop data and track ID
-drops = trimmedData_final(:,5);
-trackIDs = trimmedData_final(:,1);
-time = trimmedData_final(:,2);
-
-
-
-
-% 4. count number of drops per track
-
-% associate track ID to each drop
-dropIDs = trackIDs(drops == 1); 
-
-% count drops per ID
-unique_dropIDs = unique(dropIDs);
-drop_counts_per_ID = histc(dropIDs,unique_dropIDs);
-
-% IDs with at least 2 drops
-atLeast2 = find(drop_counts_per_ID == 2);
-
-
-
-
-% 5. for each track with at least two drops
 divisionTimes = [];
-for tr = 1:length(atLeast2)
+
+for i = 1:length(experiments)
     
-    % isolate drop data
-    tr_drops = drops(trackIDs == unique_dropIDs( atLeast2(tr) ));
+    i
+    clearvars -except i experiments divisionTimes;
     
-    % get timestamp at all drops (when drop == 1)
-    tr_time = time(trackIDs == unique_dropIDs( atLeast2(tr) ));
-    drop_times = tr_time(tr_drops == 1);
+    % 1. initialize data matrix
+    newFolder = strcat('/Users/jen/Documents/StockerLab/Data/',experiments{i},'  (t300)');
+    cd(newFolder);
+    load(['dm-t300-',experiments{i},'.mat']);
     
-    % calculate time duration between drops
-    tr_divTimes = diff(drop_times);
+    load('meta.mat');
+    meta = importdata('meta.mat');
     
-    % concatenate array of all div times per condition
-    divisionTimes = [divisionTimes; tr_divTimes]; %in hours
+
+    
+    % 2. isolate condition of interest
+    c = 3;
+    matrix_dims = size(dataMatrix);
+    trimmedData = dataMatrix(dataMatrix(:,matrix_dims(2)) == c,:);
+    
+    
+    
+    % 3. isolate time period of interest
+    minTime = meta(c,3); % based on where growth rate seems to stabilize
+    maxTime = meta(c,4);
+    
+    % remove data from timepoints earlier than minTime
+    trimmedData2 = trimmedData(trimmedData(:,2) >= minTime,:);
+    
+    % remove data from timepoints later than maxTime
+    trimmedData_final = trimmedData2(trimmedData2(:,2) <= maxTime,:);
+    
+    
+    
+    % 4. collect drop data and track ID
+    drops = trimmedData_final(:,5);
+    trackIDs = trimmedData_final(:,1);
+    time = trimmedData_final(:,2);
+    
+    
+    
+    % 5. count number of drops per track
+    
+    % associate track ID to each drop
+    dropIDs = trackIDs(drops == 1);
+    
+    % count drops per ID
+    unique_dropIDs = unique(dropIDs);
+    drop_counts_per_ID = histc(dropIDs,unique_dropIDs);
+    
+    % IDs with at least 2 drops
+    atLeast2 = find(drop_counts_per_ID == 2);
+    
+    
+    
+    
+    % 6. for each track with at least two drops
+    for tr = 1:length(atLeast2)
+        
+        % isolate drop data
+        tr_drops = drops(trackIDs == unique_dropIDs( atLeast2(tr) ));
+        
+        % get timestamp at all drops (when drop == 1)
+        tr_time = time(trackIDs == unique_dropIDs( atLeast2(tr) ));
+        drop_times = tr_time(tr_drops == 1);
+        
+        % calculate time duration between drops
+        tr_divTimes = diff(drop_times);
+        
+        % concatenate array of all div times per condition
+        divisionTimes = [divisionTimes; tr_divTimes]; %in hours
+    end
+    
+    
+    
+    % 7. build PDF from completed div time array
+    inMinutes = divisionTimes*60;
+    
+    figure(i)
+    histogram(inMinutes,'Normalization','pdf','BinWidth',5);
+    xlabel('Division Time (min)')
+    ylabel('PDF')
+    legend('0.5 uM glucose');
+    
 end
-
-
-
-% 6. build PDF from completed div time array
-inMinutes = divisionTimes*60;
-
-figure()
-histogram(inMinutes,'Normalization','pdf','BinWidth',10);
-xlabel('Division Time (min)')
-ylabel('PDF')
-legend('0.5 uM glucose');  
-
 %%
-%           7. repeat for any other conditions of interest
+
