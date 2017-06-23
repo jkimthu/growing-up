da2349cfa426eddf3e5fbf02d1b2280344e70bc2
Jen Nguyen <jknguyen@mit.edu>
2017-06-23 16:07:27 +0200

    color code cells by track ID presence in trimming stages, first pass with xy 52 from 2017-06-12

--- dynamicOutlines.m
+++ dynamicOutlines.m
@@ -6,15 +6,18 @@
 % Strategy:
 %           0. initialize tracking and image data
 %           1. isolate ellipse data from movie (stage xy) of interest
-%           2. for each image, initialize current image
-%                   3. define major axes, centroids, angles, growth rates
-%                   4. draw ellipses from image
-%                   5. color based on growth rate
-%                   6. save and display
-%           7. 
+%           2. identify tracks in rejects. all others are tracked.
+%           3. for each image, initialize current image
+%                   4. define major axes, centroids, angles, growth rates
+%                   5. draw ellipses from image, color based on tracked vs trimmed:
+%                           i. if track number is found in reject list,
+%                              color red
+%                          ii. else, color green
+%                   6. display and save
+%           7. woohoo!
 
 
-% last edit: jen, 2017 Jun 21
+% last edit: jen, 2017 Jun 23
 
 % OK LEZ GO!
 %%
@@ -24,11 +27,6 @@ clear
 clc
 experiment = '2017-06-12';
 
-% movie (xy position) of interest
-n = 52;
-img_prefix = strcat('letstry-2017-06-12_xy', num2str(n), 'T'); 
-img_suffix = 'XY1C1.tif';
-
 
 % TRACKING DATA
 % open folder for experiment of interest
@@ -36,10 +34,23 @@ newFolder = strcat('/Users/jen/Documents/StockerLab/Data/',experiment);
 cd(newFolder);
 
 % import tracking data
+load('dm-2017-06-12_untrimmed.mat');
+dataMatrix_untrimmed = dataMatrix;
+
 load('dm-2017-06-12.mat');
+dataMatrix_trimmed = dataMatrix;
 
+load('letstry-2017-06-12-dSmash.mat','T');
 
+clear dataMatrix;
+
+%%
 % IMAGE DATA
+% movie (xy position) of interest
+n = 52;
+img_prefix = strcat('letstry-2017-06-12_xy', num2str(n), 'T'); 
+img_suffix = 'XY1C1.tif';
+
 % open folder for images of interest (one xy position of experiment)
 img_folder=strcat('xy', num2str(n));
 cd(img_folder);
@@ -59,33 +70,48 @@ clear img_folder img_prefix img_suffix experiment newFolder img_folder
 
 
 % 1. isolate ellipse data from movie (stage xy) of interest
-dm_currentMovie = dataMatrix(dataMatrix(:,31) == n,:); % col 31 is movie number (n)
+dm_currentMovie_untrimmed = dataMatrix_untrimmed(dataMatrix_untrimmed(:,31) == n,:); % col 31 is movie number (n)
+dm_currentMovie_trimmed = dataMatrix_trimmed(dataMatrix_trimmed(:,31) == n,:);
 
+% 2. define IDs for tracked vs trimmed tracks
+survivorTracks = unique(dm_currentMovie_trimmed(:,1)); % col 1 = track IDs
+
+% build data matrix of rejected tracks
+rejects_currentMovie = rejectD(:,n);
+dm_currentMovie_rejects = buildDM(rejects_currentMovie,T);
 %%
 
+% sort by trim stage number
+for stage = 1:length(rejects_currentMovie)
+    
+    dm_currentRejects = dm_currentMovie_rejects(dm_currentMovie_rejects(:,31) == stage,:); %not in D2
+    lostTracks{stage} = unique(dm_currentRejects(:,1));
+
+end
+%%
 
 % for each image
 for img = 1:length(names)
     
     cla
     
-    % 2. initialize current image
+    % 3. initialize current image
     I=imread(names{img});
-    filename = strcat('dynamicOutlines-frame',num2str(img),'.tif');
+    filename = strcat('dynamicOutlines-frame',num2str(img),'-trackedVtrimmed.tif');
     
     figure(1)
     imshow(I, 'DisplayRange',[3200 7400]);
     
     
     % 3. if no tracked cells, save and skip
-    if sum(dm_currentMovie(:,30) == img) == 0
+    if sum(dm_currentMovie_untrimmed(:,30) == img) == 0
         saveas(gcf,filename)
         
         continue
         
     else
-        % 3. else, isolate data for each image
-        dm_currentImage = dm_currentMovie(dm_currentMovie(:,30) == img,:);
+        % 4. else, isolate data for each image
+        dm_currentImage = dm_currentMovie_untrimmed(dm_currentMovie_untrimmed(:,30) == img,:); % col 30 = frame #
         
         % axes
         majorAxes = dm_currentImage(:,3); % lengths
@@ -101,22 +127,75 @@ for img = 1:length(names)
         % growth rates (mu)
         mus = dm_currentImage(:,18); % mu calculated from Va
         
-        % frame
+        % frames
         frames = dm_currentImage(:,30);
         
+        % trackIDs
+        IDs = dm_currentImage(:,1);
         
         
-        % 4. for each particle in current image, draw ellipse
+        % 5. for each particle in current image, draw ellipse
         for p = 1:length(majorAxes)
             
             
-            [outline1, outline2] = drawEllipse(p,majorAxes, minorAxes, centroid_X, centroid_Y, conversionFactor);
+            [x_rotated, y_rotated] = drawEllipse(p,majorAxes, minorAxes, centroid_X, centroid_Y, angles, conversionFactor);
             
-            hold on
-            plot(outline1,outline2,'lineWidth',2)
-            xlim([0 2048]);
-            ylim([0 2048]);
+            % i. if track number is a surviving track, plot green
+            if any(IDs(p)==survivorTracks) == 1
+                
+                hold on
+                plot(x_rotated,y_rotated,'g','lineWidth',2)
+                xlim([0 2048]);
+                ylim([0 2048]);
+                
+            % ii. if track number was trimmed in first stage (size), plot blue
+            elseif any(IDs(p)== lostTracks{1}) == 1
+                hold on
+                plot(x_rotated,y_rotated,'m','lineWidth',2)
+                xlim([0 2048]);
+                ylim([0 2048]);
+                
+            % iii. if track number was trimmed in second stage (golden ratio), plot orange
+            elseif any(IDs(p)== lostTracks{2}) == 1
+                hold on
+                plot(x_rotated,y_rotated,'b','lineWidth',2)
+                xlim([0 2048]);
+                ylim([0 2048]);
+                
+            % iv. if track number was trimmed in third stage (jumpy), plot white
+            elseif any(IDs(p)== lostTracks{3}) == 1 && any(IDs(p)== lostTracks{4}) == 1
+                hold on
+                plot(x_rotated,y_rotated,'r','lineWidth',2)
+                xlim([0 2048]);
+                ylim([0 2048]);
+                
+                
+            % iv. if track number was trimmed in fourth stage (too short), plot red
+            elseif any(IDs(p)== lostTracks{4}) == 1
+                hold on
+                plot(x_rotated,y_rotated,'w','lineWidth',2)
+                xlim([0 2048]);
+                ylim([0 2048]);
+                
+                
+            % v. if track number was trimmed in fifth stage (too swiggly), plot yellow
+            elseif any(IDs(p)== lostTracks{5}) == 1
+                hold on
+                plot(x_rotated,y_rotated,'y','lineWidth',2)
+                xlim([0 2048]);
+                ylim([0 2048]);
+                
+            % vi. other
+            else
+                hold on
+                plot(x_rotated,y_rotated,'c','lineWidth',2)
+                xlim([0 2048]);
+                ylim([0 2048]);
+                
+            end
         end
+        
+        % 6. save
         saveas(gcf,filename)
         
     end
