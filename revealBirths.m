c06920272810422dc41884d837d97dd52c8dbf9b
Jen Nguyen <jknguyen@mit.edu>
2017-09-25 14:11:03 +0200

    revised to use buildDM. plot birth events (normalized by cell count) per time bin, as a scatter plot and as a histogram

--- revealBirths.m
+++ revealBirths.m
@@ -1,7 +1,7 @@
 %% revealBirths
 
 
-% Goal: Reveal histogram of birth events in growth data.
+% Goal: Reveal histogram of birth events over time, normalized by current cell count.
 
 %       Like nSync.m and spin-offs, this script looks for cell cycle synchronization.
 %       Unlike the others, this is a simple glance at the distribution of
@@ -9,20 +9,12 @@
    
 
 
-%  Last edit: Jen Nguyen, February 23nd 2016
+%  Last edit: Jen Nguyen, 2017 Sept 25
 
 
 
-% The intended input for these scripts is the following data matrix,
-% saved with the naming convention of:
-
-% dmMMDD-cond.mat
-
-%      where,
-%              dm  =  dataMatrix                  (see matrixBuilder.m)
-%              MM  =  month of experimental date
-%              DD  =  day of experimental date
-%       condition  =  experimental condition      (fluc or const)
+% As of Sept 2017, this script uses function, buildDM, instead of specific
+% matrixBuilder.m data outputs. The overall concept is still similar.
 
 
 % In these matrices is a column (#5) of boolean values:
@@ -33,72 +25,100 @@
 
 % Strategy:
 %
-%      0.  initialize data and parameters
-%      1.  find indices where drop? == 1
-%      2.  match indices with corresponding timestamp
-%      3.  count events within incremental bins of time
-%      4.  plot as a histogram of event counts vs. time
-
+%      0.  initialize data and binning parameters
+%      1.  specify current condition of interest
+%               2.  isolate data from current condition
+%               3.  accumulate drops (division events) by time bin
+%               4.  convert bin # to absolute time
+%               5.  count birth events per timebin
+%               6.  count number of tracks per timebin
+%               7.  plot scatter over time
+%               8.  plot histogram of normalized birth events over time
+%      9.  repeat for all conditions
 
 % OK! Lez go!
 
 
 %%
-%   Initialize.
+% 0. initialize data
 
-% data
-dmDirectory = dir('dm*.mat'); % note: this assumes the only two data matrices are 'const' and 'fluc'
-names = {dmDirectory.name}; % loaded alphabetically
+clc
+clear
 
-for dm = 1:length(names)
-    load(names{dm});                
-    dataMatrices{dm} = dataMatrix;                                         % for entire condition
-end                                                                        
+% trimmed dataset
+load('lb-monod-2017-09-20-jiggle-0p1.mat','D5','T');
+dataMatrix = buildDM(D5,T);
 
-% parameters
-expHours = 10; %  duration of experiment in hours                      
-binFactor = 200; % time bins of 0.005 hr  
-hrPerBin = 1/binFactor; 
+% 0. initialize binning parameters
+expHours = 10;          % duration of experiment in hours                      
+binFactor = 20;         % bins per hour
+hrPerBin = 1/binFactor; % hour fraction per bin
 
-clear dataMatrix dmDirectory dm;
-clear names;
 
-%
-%   Find and plot birth events.
+%%
+% 1. specify current condition of interest
+totalCond = max(dataMatrix(:,35)); % col 35 = condition value
 
-for condition = 1:2
-  
-    interestingData = dataMatrices{condition};  % condition: 1 = constant, 2 = fluctuating
-    timestamps = interestingData(:,2);
-    drops = interestingData(:,5);                                          
+for condition = 1:totalCond
     
-    % accumulate drops by time bin
+    % 2. isolate data from current condition
+    interestingData = dataMatrix(dataMatrix(:,35) == condition,:);
+
+    % 3. accumulate drops by time bin
+    drops = interestingData(:,5);
+    timestamps = interestingData(:,2)/3600; % time in seconds converted to hours
     timeBins = ceil(timestamps*binFactor);                
-    binnedByTime = accumarray(timeBins,drops,[],@(x) {x});                     
+    binnedDrops = accumarray(timeBins,drops,[],@(x) {x});  
     
-    % convert bin # to absolute time
-    timeVector = linspace(1, expHours/hrPerBin, expHours/hrPerBin);         
-    timeVector = hrPerBin*timeVector';                                         
+    % 4. accumulate track count by time bin
+    %tracks = interestingData(:,34); % col 34 = total track number (not track ID as assigned during particle tracking by individual movies)
+    %binnedTracks = accumarray(timeBins,tracks,[],@(x) {x});
     
-    % count birth events per timebin
-    birthEvents = zeros(binFactor*expHours,1);
-    for i = 1:length(binnedByTime)
+    % 4. convert bin # to absolute time
+    %timeVector = linspace(1, expHours/hrPerBin + 1, expHours/hrPerBin + 1);         
+    timeVector = linspace(1, max(timeBins), max(timeBins));
+    timeVector = hrPerBin*timeVector';  
+    
+    % 5. count birth events per timebin
+    birthEvents = zeros(max(timeBins),1);
+    for i = 1:length(binnedDrops)
         
-        birthEvents(i) = sum(binnedByTime{i});                                  
-        
+        birthEvents(i) = sum(binnedDrops{i});                                  
+          
     end
     clear i;
     
-    % normalize with number of tracks present in at timepoint
-    numTracks = cell2mat(cellfun(@length,binnedByTime,'UniformOutput',false));
-    numTracks(binFactor*expHours,1) = 0;
+    % 6.  count number of tracks per timebin
+    numTracks = cell2mat(cellfun(@length,binnedDrops,'UniformOutput',false));
+    
+    % 6. normalize with number of tracks present in at timepoint
+    %numTracks(binFactor*expHours,1) = 0;
     birthHisto = birthEvents./numTracks;
     
+    % 7. plot scatter vs. time
     figure(1)
-    subplot(2,1,condition)
-    bar(timeVector,birthHisto)
-    axis([0,10,0,.15])
+    plot(timeVector,birthHisto,'o')
+    axis([0,10.1,0,.35])
     hold on
+    xlabel('Time (hr)')
+    ylabel('Births per capita')
+    legend('full LB','1/2 LB','1/4 LB','1/8 LB','1/16 LB','1/32 LB'); 
     
+    % 8. plot histogram of normalized birth events vs time
+    figure(2)
+    subplot(totalCond,1,condition)
+    bar(timeVector,birthHisto)
+    axis([0,10.1,0,.4])
+    hold on
+    grid on
+    xlabel('Time (hr)')
+    ylabel('Births per capita')
+    legend(num2str(condition)); 
+    
+    clear interestingData drops timestamps timeBins binnedDrops timeVector
+    clear birthEvents numTracks birthHisto
+    
+    % 9.  repeat for all conditions
 end
+
 
