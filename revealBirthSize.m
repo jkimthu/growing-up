bfebbf5256d16cb160754de0c296816aebc5dc81
Jen Nguyen <jknguyen@mit.edu>
2017-10-20 14:49:30 +0200

    constrain analysis to time windows of stabilized growth. this update applies to 2017-10-10 data

--- revealBirthSize.m
+++ revealBirthSize.m
@@ -7,7 +7,7 @@
    
 
 
-%  Last edit: Jen Nguyen, 2017 Oct 6
+%  Last edit: Jen Nguyen, 2017 Oct 13
 
 
 
@@ -21,11 +21,12 @@
 %      0.  initialize data and binning parameters
 %      1.  specify current condition of interest
 %               2.  isolate data from current condition
-%               3.  accumulate size at birth data by timebin
-%               4.  convert bin # to absolute time
-%               5.  calculate average and s.e.m. per timebin
-%               6.  plot!
-%      7.  repeat for all conditions
+%               3.  trim data to only account for stabilized growth
+%               4.  accumulate size at birth data by timebin
+%               5.  convert bin # to absolute time
+%               6.  calculate average and s.e.m. per timebin
+%               7.  plot!
+%      8.  repeat for all conditions
 
 
 % OK! Lez go!
@@ -38,8 +39,10 @@ clc
 clear
 
 % trimmed dataset
-load('lb-monod-2017-09-26-window5-jiggle-c12-0p1-c3456-0p5.mat','D5','M','T');
+load('lb-fluc-2017-10-10-window5-width1p4v1p7-jiggle-0p5-bigger1p8.mat','D5','M','T');
 dataMatrix = buildDM(D5,M,T);
+load('meta.mat');
+meta = meta_2017oct10;
 
 % 0. initialize binning parameters
 expHours = 10;          % duration of experiment in hours                      
@@ -55,8 +58,6 @@ for condition = 1:totalCond
     % 2.  isolate data from current condition
     interestingData = dataMatrix(dataMatrix(:,35) == condition,:);
     
-    % 3.  accumulate size at birth data by timebin
-    
     % i. isolate length and time data
     allLengths = interestingData(:,3); % col 3 = measured lengths (um)
     allWidths = interestingData(:,12); % col 12 = measured widths (um)
@@ -65,13 +66,31 @@ for condition = 1:totalCond
     % ii. select rows where isDrop = 1
     isDrop = interestingData(:,5);
     birthTimes = timestamps(isDrop == 1);
-    birthSizes = allLengths(isDrop == 1);
+    birthLengths = allLengths(isDrop == 1);
     birthWidths = allWidths(isDrop == 1);
     
+    
+    % 3.  trim data to only account for stabilized growth
+    
+    % i. remove data not in stabilized region
+    minTime = meta(condition,3);  % hr
+    maxTime = meta(condition,4);
+    
+    birthLengths_trim1 = birthLengths(birthTimes >= minTime);
+    birthWidths_trim1 = birthWidths(birthTimes >= minTime);
+    birthTimes_trim1 = birthTimes(birthTimes >= minTime);
+    
+    birthLengths_trim2 = birthLengths_trim1(birthTimes_trim1 <= maxTime);
+    birthWidths_trim2 = birthWidths_trim1(birthTimes_trim1 <= maxTime);
+    birthTimes_trim2 = birthTimes_trim1(birthTimes_trim1 <= maxTime);
+    
+    
+    % 4.  accumulate size at birth data by timebin
+    
     % iii. convert birthTimes into timebins
-    timeBins = ceil(birthTimes*binFactor);                
-    binnedSizes = accumarray(timeBins,birthSizes,[],@(x) {x});
-    binnedWidths = accumarray(timeBins,birthWidths,[],@(x) {x});
+    timeBins = ceil(birthTimes_trim2*binFactor);                
+    binnedSizes = accumarray(timeBins,birthLengths_trim2,[],@(x) {x});
+    binnedWidths = accumarray(timeBins,birthWidths_trim2,[],@(x) {x});
     
     
     % 4.  convert bin # to absolute time
@@ -98,31 +117,31 @@ for condition = 1:totalCond
     hold on
     xlabel('Time (hr)')
     ylabel('Length at birth + s.e.m. (min)')
-    legend('full LB','1/2 LB','1/4 LB','1/8 LB','1/16 LB','1/32 LB'); 
+    legend('fluc','1/1000 LB','ave','1/50 LB'); 
     
-    figure(2)
-    errorbar(timeVector,meanVector,stdVector)
-    axis([0,10.3,0,10])
-    hold on
-    xlabel('Time (hr)')
-    ylabel('Length at birth + standard dev (min)')
-    legend('full LB','1/2 LB','1/4 LB','1/8 LB','1/16 LB','1/32 LB'); 
+%     figure(2)
+%     errorbar(timeVector,meanVector,stdVector)
+%     axis([0,10.3,0,10])
+%     hold on
+%     xlabel('Time (hr)')
+%     ylabel('Length at birth + standard dev (min)')
+%     legend('fluc','1/1000 LB','ave','1/50 LB'); 
     
     % 7. plot pdfs from steady-state
-    stableBirthSizes = birthSizes(birthTimes > 3);
+    stableBirthLengths = birthLengths(birthTimes > 3);
     
     figure(3)
-    histogram(stableBirthSizes,'Normalization','pdf')
-    axis([0,12,0,1.2])
+    histogram(stableBirthLengths,'Normalization','pdf')
+    axis([0,12,0,1.5])
     hold on
     xlabel('Length at birth (um)')
     ylabel('pdf')
-    legend('full LB','1/2 LB','1/4 LB','1/8 LB','1/16 LB','1/32 LB');
+    legend('fluc','1/1000 LB','ave','1/50 LB'); 
     
     figure(4)
     subplot(totalCond,1,condition)
-    histogram(stableBirthSizes,'Normalization','pdf')
-    axis([0,20,0,1.2])
+    histogram(stableBirthLengths,'Normalization','pdf')
+    axis([0,20,0,1.5])
     hold on
     xlabel('Length at birth (um)')
     ylabel('pdf')
@@ -134,31 +153,31 @@ for condition = 1:totalCond
     hold on
     xlabel('Time (hr)')
     ylabel('Width at birth + s.e.m. (min)')
-    legend('full LB','1/2 LB','1/4 LB','1/8 LB','1/16 LB','1/32 LB'); 
+    legend('fluc','1/1000 LB','ave','1/50 LB');
     
-    figure(6)
-    errorbar(timeVector,meanWidth,stdWidth)
-    axis([0,10.3,0.75,2.25])
-    hold on
-    xlabel('Time (hr)')
-    ylabel('Width at birth + standard dev (min)')
-    legend('full LB','1/2 LB','1/4 LB','1/8 LB','1/16 LB','1/32 LB'); 
+%     figure(6)
+%     errorbar(timeVector,meanWidth,stdWidth)
+%     axis([0,10.3,0.75,2.25])
+%     hold on
+%     xlabel('Time (hr)')
+%     ylabel('Width at birth + standard dev (min)')
+%     legend('fluc','1/1000 LB','ave','1/50 LB');
     
     % 7. plot pdfs from steady-state
     stableBirthWidths = birthWidths(birthTimes > 3);
     
     figure(7)
     histogram(stableBirthWidths,'Normalization','pdf')
-    axis([1,2,0,10])
+    axis([0.8,1.8,0,8])
     hold on
     xlabel('Width at birth (um)')
     ylabel('pdf')
-    legend('full LB','1/2 LB','1/4 LB','1/8 LB','1/16 LB','1/32 LB');
+    legend('fluc','1/1000 LB','ave','1/50 LB');
     
     figure(8)
     subplot(totalCond,1,condition)
     histogram(stableBirthWidths,'Normalization','pdf')
-    axis([1,2,0,10])
+    axis([0.8,1.8,0,9])
     hold on
     xlabel('Width at birth (um)')
     ylabel('pdf')
