99fa06b3af218fe8609bb2697ab8e62cd165b35c
Jen Nguyen <jknguyen@mit.edu>
2017-11-27 13:04:28 +0100

    edit script to use new data structures, see storedMetaData. this uses meta data to compile mu data and plot it for individual data points (instantaneous) as well as data weigthed by track ID

--- plotMonod.m
+++ plotMonod.m
@@ -5,110 +5,240 @@
 % strategy:
 %
 %       0. initialize experiment data
-%       1. for each experiment...load data
-%               2. for each condition in experiment...
+%       1. for each experiment, load data
+%               2. for each condition in experiment:
 %                       3. isolate condition specific Mu_va and time data
 %                       4. remove mu data with timestamps prior to and after stabilization
 %                       5. remove zeros (always two at start and end of track) and negatives
 %                       6. calculate mean and sem
-%                       7. plot ave mu_va vs concentration
-
-%                       5. plot mu vs log(concentration)
-%               6. repeat for all conditions
-%       7.  repeat for all experiments
+%                       7. store mean and conc data for fitting
+%                       8. plot ave mu_va vs concentration
+%                       9. plot ave mu_va vs log(concentration)
+%                      10. calculate and plot fit curve
+%              11. repeat for all conditions
+%      12.  repeat for all experiments
 %   
 
 
-% last updated: 2017 October 23
+% last updated: 2017 November 24
 
-%%
+%% 0. initialize experiment data
 clear
 clc
 
-% 0. initialize experiment data
-experimentList = {
-    '2017-09-26';
-    '2017-10-10';
-    };
+cd('/Users/jen/Documents/StockerLab/Data_analysis/')
+load('storedMetaData.mat')
 
+dataIndex = find(~cellfun(@isempty,storedMetaData));
+measuredData = cell(size(storedMetaData));
 
-dataList = {
-    'lb-monod-2017-09-26-window5-va-jiggle-c12-0p1-c3456-0p5-bigger1p8.mat';
-    'lb-fluc-2017-10-10-window5-va-width1p4v1p7-jiggle-0p5-bigger1p8.mat';
-    };
-
-concentrationList = {
-    % each col = a condition
-    % each row = an experiment
-    1, 1/8, 1/32, 1/100, 1/1000, 1/10000;       % 2017-09-26: all stable envir
-    105/10000, 1/1000, 105/10000, 1/50, [], []; % 2017-10-10: fluc (5min), low, ave, high
-    };
-
-conditionsList = {
-    % each col = a condition
-    % each row = an experiment
-    1:10, 11:20, 21:30, 31:40, 41:50, 51:60; % 2017-09-26: all stable envir
-    1:10, 11:20, 21:30, 31:40, [], [];       % 2017-10-10: fluc (5min), low, ave, high  
-    };
-
-stablePeriods = { 
-    % each cell = an experiment
-    % first col = start, second col = end
-    [2, 4.5; 2, 10; 2, 10; 3.5, 10; 3, 10; 3, 10];             % 2017-09-26: all stable envir
-    [2.5, 6.5; 4, 7.5; 2.5, 10; 2.5, 10; NaN, NaN; NaN, NaN];  % 2017-10-10: fluc (5min), low, ave, high       
-    };
-                   
-               
-%%
-% 1. for each experiment
-experimentCount = length(dataList);
-for e = 1:experimentCount
+%% 1. for each experiment, load data
+for e = 1:length(dataIndex)
     
-    % 1. open corresponding directory
-    newFolder = strcat('/Users/jen/Documents/StockerLab/Data/LB/',experimentList{e});%,'  (t300)');
-    cd(newFolder);
+    % identify experiment by date
+    index = dataIndex(e);
+    date = storedMetaData{index}.date;
     
-    % 1. load experiment data
-    load(dataList{e},'D5','M','M_va','T');
-    currentStabilizedPeriods = stablePeriods{e};
+    % move directory to experiment data
+    experimentFolder = strcat('/Users/jen/Documents/StockerLab/Data/LB/',date);
+    cd(experimentFolder)
     
+    % load data
+    timescale = storedMetaData{index}.timescale;
+    if ischar(timescale) == 0
+        filename = strcat('lb-fluc-',date,'-window5-width1p4-1p7-jiggle-0p5.mat');
+    elseif strcmp(date,'2017-09-26') == 1
+        filename = 'lb-monod-2017-09-26-window5-va-jiggle-c12-0p1-c3456-0p5-bigger1p8.mat';
+    elseif strcmp(date, '2017-11-09') == 1
+        filename = 'lb-control-2017-11-09-window5-width1p4-jiggle-0p5.mat';
+    end
+    load(filename,'D5','M','M_va','T')
+    
+    % build experiment data matrix
+    display(strcat('Experiment (', num2str(e),') of (', num2str(length(dataIndex)),')'))
     exptData = buildDM(D5,M,M_va,T);
     
-    % 2. for each condition in experiment
-    conditions = find(~cellfun(@isempty,conditionsList(e,:)));
+    clear D5 M M_va T filename experimentFolder
     
-    for c = 1:max(conditions)
+    %% 2. for each condition in experiment, calculate mean mu and error
+    xys = storedMetaData{index}.xys;
+    xy_dimensions = size(xys);
+    totalConditions = xy_dimensions(1);
+    
+    % initialize structures for data storage
+    compiledTrackMeans = cell(1,totalConditions);
+    compiledIndividualStats= cell(1,totalConditions);
+    
+    for c = 1:totalConditions
         
-        % 3. isolate condition specific mu_va and time data
-        conditionStabilizedPeriod = currentStabilizedPeriods(c,:);
-        conditionData = exptData(exptData(:,35) == c,:); % col 35 = condition
-        %Mus = conditionData(:,4); % col 4 = mus
-        Mus_va = conditionData(:,18); % col 18 = calculated mu_vals
-        Time = conditionData(:,2)/3600; % col 2 = timestamps in sec, covert to hr
-        
+        % 3. isolate condition specific Mu_va and time data
+        condition = exptData(exptData(:,35) == c,:); % col 35 = condition
+        mu_va = condition(:,18); % col 18 = calculated mu_vals
+        time = condition(:,2)/3600; % col 2 = timestamps in sec, covert to hr
+        track = condition(:,34); % col 34 = track count, not ID from tracking
+        clear condition
         
         % 4. remove mu data with timestamps prior to and after stabilization
-        Mus_trim1 = Mus_va(Time >= conditionStabilizedPeriod(1));
-        Time_trim1 = Time(Time >= conditionStabilizedPeriod(1));
-        %plot(Time_trim1,Mus_trim1,'o')
+        haltTimes = storedMetaData{index}.bubbletime;
         
-        Mus_trim2 = Mus_trim1(Time_trim1 <= conditionStabilizedPeriod(2));
-        Time_trim2 = Time_trim1(Time_trim1 <= conditionStabilizedPeriod(2));
-        %plot(Time_trim2,Mus_trim2,'o')
+        mus_trim1 = mu_va(time >= 3);  % consider only timepoints after 3 hrs
+        time_trim1 = time(time >= 3);
+        track_trim1 = track(time >= 3);
         
+        if haltTimes(c) > 0
+            mus_trim2 = mus_trim1(time_trim1 <= haltTimes(c));
+            %time_trim2 = time_trim1(time_trim1 <= haltTimes(c));
+            track_trim2 = track_trim1(time_trim1 <= haltTimes(c));
+        else
+            mus_trim2 = mus_trim1;
+            track_trim2 = track_trim1;
+        end
         
         % 5. remove zeros (always two at start and end of track) and negatives
-        Mus_trim3 = Mus_trim2(Mus_trim2 > 0);
-        
+        mus_trim3 = mus_trim2(mus_trim2 > 0);
+        track_trim3 = track_trim2(mus_trim2 > 0);
         
         % 6. calculate mean and sem
-        muMean = mean(Mus_trim3);
-        muStd = std(Mus_trim3);
-        muCounts = length(Mus_trim3);
+        % i. for all individual (but not independent) points
+        muMean = mean(mus_trim3);
+        muStd = std(mus_trim3);
+        muCounts = length(mus_trim3);
         muSem = muStd./sqrt(muCounts);
         
         
-        % 7. plot ave mu_va vs concentration
+        % ii. for each track, then calculate condition mean (weighted by track)
+        
+        % determine length of each track (post-trimming)
+        [a,trackID]=hist(track_trim3,unique(track_trim3));
+        trackLength = a';
+        
+        % limit analysis to tracks >= 30 points long
+        finalTracks = trackID(trackLength>=10);
+        
+        % calculate mean for each track
+        trackMeans = zeros(length(finalTracks),1);
+        for t = 1:length(finalTracks)
+            currentTrack = finalTracks(t);
+            trackMus = mus_trim3(track_trim3==currentTrack);
+            trackMeans(t) = mean(trackMus);
+        end
+        clear finalTracks trackMus
+        
+        % 7. accumulate data for storage
+        compiledTrackMeans{c} = trackMeans;
+        compiledIndividualStats{c}.muMean = muMean; 
+        compiledIndividualStats{c}.muStd = muStd;
+        compiledIndividualStats{c}.muCounts = muCounts;
+        compiledIndividualStats{c}.muSem = muSem;
+        
+        clear track_trim1 track_trim2 track_trim3 mus_trim1 mus_trim2 mus_trim3
+        clear time_trim1 time_trim2 trackLength trackID a currentTrack
+    end
+    
+    % 8. store data from all conditions into measured data structure
+    measuredData{index}.tracks = compiledTrackMeans;
+    measuredData{index}.individuals = compiledIndividualStats;
+    
+end
+%% 9. save stored data into measured data structure
+cd('/Users/jen/Documents/StockerLab/Data_analysis/')
+save('measuredData.mat','measuredData')
+
+%% 10. plot ave mu_va vs concentration
+clc
+clear
+
+load('storedMetaData.mat')
+load('measuredData.mat')
+dataIndex = find(~cellfun(@isempty,storedMetaData));
+
+% for each experiment, load data
+for e = 1:length(dataIndex)
+    
+    % identify experiment by date
+    index = dataIndex(e);
+    date{e} = storedMetaData{index}.date;
+    
+    % load timescale
+    timescale = storedMetaData{index}.timescale;
+    
+    % isolate mu data for current experiment
+    data_individuals = measuredData{index}.individuals;
+    data_tracks = measuredData{index}.tracks;
+    
+    % isolate concentration data for current experiment
+    concentration = storedMetaData{index}.concentrations;
+    
+    % plot individual data, labeled by experiment date
+    figure(1)
+    for c = 1:length(concentration)
+        h(e) = errorbar(log(concentration(c)), data_individuals{c}.muMean, data_individuals{c}.muSem,'o','Color',[0 1 1]*e*.1,'MarkerSize',10);
+        hold on
+        legend(h(:),date)
+    end
+    xlabel('mu, individual (1/hr)')
+    ylabel('log fold LB dilution')
+    
+    % plot individual data, labeled by stable vs fluc
+    figure(2)
+    for c = 1:length(concentration)
+        % if fluc experiment
+        if ischar(timescale)
+            errorbar(log(concentration(c)), data_individuals{c}.muMean, data_individuals{c}.muSem,'o','Color','k','MarkerSize',10);
+            hold on
+        elseif timescale == 30 && c == 1
+            errorbar(log(concentration(c)), data_individuals{c}.muMean, data_individuals{c}.muSem,'o','Color',[0.25 0.25 0.9],'MarkerSize',10);
+            hold on
+        elseif timescale == 300 && c == 1
+            errorbar(log(concentration(c)), data_individuals{c}.muMean, data_individuals{c}.muSem,'o','Color',[0 .7 .7],'MarkerSize',10);
+            hold on
+            legend('5 min')
+        elseif timescale == 900 && c == 1
+            errorbar(log(concentration(c)), data_individuals{c}.muMean, data_individuals{c}.muSem,'o','Color',[1 0.6 0],'MarkerSize',10);
+            hold on
+        else
+            errorbar(log(concentration(c)), data_individuals{c}.muMean, data_individuals{c}.muSem,'o','Color','k','MarkerSize',10);
+            hold on
+        end
+    end
+    legend('30 sec','5 min','15 min','stable')
+    xlabel('mu, individual (1/hr)')
+    ylabel('log fold LB dilution')
+    
+    % plot track data, labeled by experiment date
+    figure(3)
+    expMeans = cellfun(@mean,data_tracks);
+    expStds = cellfun(@std,data_tracks);
+    expCounts = cellfun(@length, data_tracks);
+    expSems = expStds./sqrt(expCounts);
+    
+    for c = 1:length(concentration)
+        if ~isempty(data_tracks{c})
+            p(e) = errorbar(log(concentration(c)), expMeans(c), expSems(c),'o','Color',[0 1 1]*e*.1,'MarkerSize',10);
+            hold on
+            legend(p(:),date)
+        else
+            continue
+        end
+        axis([-10 2 0 4])
+    end
+    xlabel('mu, track (1/hr)')
+    ylabel('log fold LB dilution')
+    
+end
+
+%
+%                      10. calculate and plot fit curve (model: quadratic)
+%              11. repeat for all conditions
+%      12.  repeat for all experiments
+               
+%%
+
+    
+    for c = 1:max(conditions)
+ 
+        
+        % 8. plot ave mu_va vs concentration
         figure(1)
         if e == 1
             errorbar(concentrationList{e,c},muMean,muSem,'o','Color',[0 0.7 0.7],'MarkerSize',10)
@@ -123,6 +253,28 @@ for e = 1:experimentCount
             hold on 
         end
         legend('expt1-1','expt1-1/8','expt1-1/32','expt1-1/100','expt1-1/1000','expt1-1/10000','fluc1','low1','ave1','high1')
+        
+        % 9. plot ave mu_va vs log(concentration)
+        figure(2)
+        if e == 1
+            errorbar(log10(concentrationList{e,c}),muMean,muSem,'o','Color',[0 0.7 0.7],'MarkerSize',10)
+            axis([-4,0.1,0,4])
+            grid on
+            hold on
+            xlabel('log concentration (Fraction LB)')
+            ylabel('doubling rate of volume (hr-1)')
+        else
+            errorbar(log10(concentrationList{e,c}),muMean,muSem,'o','Color',[1 0.6 0],'MarkerSize',10)
+            axis([-4,0.1,0,4])
+            hold on 
+        end
+        legend('expt1-1','expt1-1/8','expt1-1/32','expt1-1/100','expt1-1/1000','expt1-1/10000','fluc1','low1','ave1','high1')
+
+        
     end
     clear exptData M D5 T
 end
+%%
+% 10. calculate and plot fit (model: quadratic)
+qFit = fit(x,y,'smoothingspline','Exclude',2);
+figure(4); hold on; plot(qFit,x,y);
