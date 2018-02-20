2f975b2a253f17114a520c524e7bfa45e1f3156a
Jen Nguyen <jknguyen@mit.edu>
2018-02-20 12:38:43 +0100

    update names for intialization of 40x image series and timestamps

--- calculateFluoresceinSignal.m
+++ calculateFluoresceinSignal.m
@@ -8,15 +8,15 @@
 %                  ii. channel only crop of junc at end
 %           1. for each image series, go to directory and initialize images
 %                   2. for each image in current series, load image
-%                          3. calculate mean pixel intensity in x direction and timestamp
-%                          4. plot intensity in y direction over time
-%                          5. save plot as timepoint
-%                          6. calculate sum intensity of entire image, save
-%                   6. repeat for all images
-%           7. plot sum intensity over time
-%           8. repeat for all series, holding sum intensity data
+%                          3. calculate sum intensity of entire image, save
+%                   4. repeat for all images
+%                   5. load timestamp data
+%                   6. plot sum intensity over time
+%           7. repeat for both reference (junc) and test (xy10) signals
+%           8. save plot and output signal and timestamp data
+%           9. 9. output signal and timestamp data
 
-% last updated: jen, 2018 Feb 13
+% last updated: jen, 2018 Feb 15
 
 % commit: update names for intialization of 40x image series and timestamps
 
@@ -27,6 +27,9 @@ function [signals,timestamps] = calculateFluoresceinSignal(experiment)
 % 0. initialize image series
 
 % initialize fluctuating fluorescein image series
+exptFolder = strcat('/Users/jen/Documents/StockerLab/Data/LB/',experiment);
+cd(exptFolder);
+
 if strcmp(experiment,'2017-11-15') == 1
     timestamps = xlsread(strcat(experiment,'-timestamps-60x.xlsx'));
     series = {
@@ -37,7 +40,7 @@ else
     timestamps = xlsread(strcat('timestamps-',experiment,'-40x.xlsx'));
     series = {
         'test_start_junc_40x_crop';
-        'test_start_xy10_40x'
+        'test_start_xy10_40x_crop'
         };
 end
 clear ans
@@ -60,13 +63,8 @@ for s = 1:length(series)
     % for each image
     for img = 1:length(xyDirectory)
         
-        % 1. read in image
+        % 2. read in image
         I=imread(names{img});
-        
-        % 2. calculate mean pixel intensity in x direction and timestamp
-        meanIntensity = mean(I,2);
-        seriesTimestamps = timestamps(:,s);
-        timeVector = seriesTimestamps(~isnan(seriesTimestamps));
         
         % 3. calculate sum intensity of entire image, save sum in vector
         sumIntensity_x = sum(I,2);
@@ -75,19 +73,27 @@ for s = 1:length(series)
         % 4. repeat for all images
     end
     
-    % 5. plot raw intensity over time
+    % 5. load timestamps
+    seriesTimestamps = timestamps(:,s);
+    timeVector = seriesTimestamps(~isnan(seriesTimestamps));
+    
+    % 6. plot raw intensity over time
     fluoresceinSignals = figure(1);
     plot(timeVector, sumIntensity)
     hold on
     
     signals{s} = sumIntensity;
     clear sumIntensity
+    
+    % 7. repeat for both reference (junc) and test (xy10) signals
 end
 
+% 8. save plot 
 legend('start','end')
 xlabel('time (sec)')
 ylabel('raw signal intensity')
 saveas(fluoresceinSignals,strcat('fluoresceinSignals-',experiment),'epsc')
 close(fluoresceinSignals)
     
+% 9. output signal and timestamp data
 end
