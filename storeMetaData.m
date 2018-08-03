1b2840607dd5adf825961a39a9a515292d622566
Jen Nguyen <jknguyen@mit.edu>
2018-08-03 15:14:34 +0200

    added new variable 'nutrient source' to existing structure

--- storeMetaData.m
+++ storeMetaData.m
@@ -10,9 +10,13 @@
 %               5. xys per condition: each row represents a condition
 %               6. signal timestamp: first printed line of signal onset
 %               7. flow rate, in ul/min as measured from MPG
+%               8. experiment type
+%               9. nutrient source
+%              10. shift time
 %
 %       each field is stored in a structure, contained in a matrix of cells,
 %       such that:
+
 %               1. each column is a different timescale/experiment type
 %                       1. 30 sec
 %                       2. 300 sec  (5 min)
@@ -20,38 +24,38 @@
 %                       4. 3600 sec (60 min)
 %                       5. none     (monod)
 %                       6. single upshift
-%
+%                       7. single downshift
+
 %               2. each row is a different experimental replicate
 
 % strategy:
 %
 %       0. initialize dimensions of current data structure
-%       1. prompt user for timescale data
-%       2. determine location of new cell (experiment) to add to current data
-%       3. prompt user for experiment date, assign to field
-%       4. generate data structure for experiment
-%               i. designate concentrations
-%              ii. designate xy positions
-%       5. prompt user for bubbles in fluc
-%       6. prompt user for bubbles in low
-%       7. prompt user for bubbles in ave
-%       8. prompt user for bubbles in high
-%       9. assign bubble appearance times to field (bubbletime)
-%      10. prompt user for signal timestamp
-%      11. prompt user for measured flow rate through MPG 
+%       1. prompt user for experiment type
+%       2. prompt user for experiment timescale
+%       3. determine location of new cell (experiment) to add to current data
+%       4. prompt user for experiment date, assign to field
+%       5. generate data structure for experiment
+%               i. designate nutrient source
+%              ii. designate concentrations
+%             iii. designate xy positions
+%       6. prompt user for bubbles in fluc
+%       7. prompt user for bubbles in low
+%       8. prompt user for bubbles in ave
+%       9. prompt user for bubbles in high
+%      10. assign bubble appearance times to field (bubbletime)
+%      11. prompt user for signal timestamp
+%      12. prompt user for measured flow rate through MPG 
 
-%      12. assign data structure to new (experiment-specific cell)
-%      13. save stored meta data
+%      13. assign data structure to new experiment-specific cell
+%      14. save stored meta data
 
-%      14. add new variables to previously saved cells:
-%                - for strategy, scroll to section
-
-%      15. add x coordinates to previously saved cells
-%                - for strategy, scroll to section
+%      15. add new variables to previously saved data entries:
+%                - for specific strategy, scroll to section
 
 
 % last updated: 2018 August 03
-% commit message: added new variable 'experiment type' to existing structure 
+% commit message: added new variable 'nutrient source' to existing structure 
 
 
 % OK let's go!
@@ -102,15 +106,26 @@ metadata(1).date = date;
 
 
 % 4. generate data structure to assign to new cell
-%    i. designate concentrations
-lowConc = 1/1000;
-aveConc = 105/10000;
-highConc = 1/50;
-flucConc = aveConc;
-metadata(1).concentrations = [flucConc; lowConc; aveConc; highConc];
+%    i. designate nutrient source
+prompt = 'Enter nutrient source as string (LB/glucose): ';
+nutrientSource = input(prompt);
+metadata(1).nutrient = nutrientSource;
+
+%   ii. designate concentrations
+if strcmp(nutrientSource,'LB') == 1
+    lowConc = 1/1000;
+    aveConc = 105/10000;
+    highConc = 1/50;
+    flucConc = aveConc;
+    metadata(1).concentrations = [flucConc; lowConc; aveConc; highConc];
+else
+    alternative = 'Enter concentrations as vector ([c1; c2; c3;...]): ';
+    altConcentrations = input(alternative);
+    metadata(1).concentrations = altConcentrations;
+end
 
 
-%   ii. designate xy positions
+%  iii. designate xy positions
 xyfluc = 1:10;
 xylow = 11:20;
 xyave = 21:30;
@@ -168,13 +183,15 @@ prompt = 'Enter row number of new experiment replicate: ';
 row = input(prompt);
 storedMetaData{row,column} = metadata;
 
+
 %% 13. save storedMetaData
 save('storedMetaData.mat','storedMetaData')
+
 
 %% 14. add new variable to pre-existing cell of experiment meta data
 
 % last used: jen, 2018 August 03
-% for adding experimentType to all existing datasets
+% for adding nutrient source to all existing datasets
 
 
 % strategy:
@@ -210,12 +227,12 @@ for e = 1:experimentCount
     date = storedMetaData{index}.date;
       
     % 4. prompt user to enter value of new variable
-    prompt = strcat('Enter value of new variable (experimentType = origFluc/monod/upshift/downshift)', date,' : ');
-    expType = input(prompt);
+    prompt = strcat('Enter value of new variable (nutrientSource = LB/glucose) for .', date,' : ');
+    nutrientSource = input(prompt);
 
     % 5. store variable into copy of meta data
     currentStructure = copyMD{index};
-    currentStructure.experimentType = expType;
+    currentStructure.nutrient = nutrientSource;
     copyMD{index} = currentStructure;
 
 end
@@ -224,66 +241,3 @@ end
 storedMetaData = copyMD;
 save('storedMetaData.mat','storedMetaData')
 
-%% 15. add x coordinates from cell positions and junc to existing data structure
-
-% last used: jen, 2018 Feb 21
-% for adding x-coordinates to all existing data sets (thru 2018-02-01)
-
-
-% strategy:
-%           0. initialize existing meta data structure
-%           1. copy data structure -- safety first!
-%           2. collect summary info for existing data
-%           3. load positions recorded on excel files
-%           4. for all experiments (cells with data),
-%                 4. print experiment date
-%                 5. assign cell positions to 10-value array and print
-%                 6. assign junc position and print
-%                 7. store variables into copy of meta data
-%           8. if satisfied, save copy with new variable as true meta data
-
-clear
-clc
-
-% 0. initialize
-cd('/Users/jen/Documents/StockerLab/Data_analysis/')
-load('storedMetaData.mat')
-
-% 1. copy data structure -- safety first!
-copyMD = storedMetaData;
-
-% 2. collect summary info for existing data
-dataIndex = find(~cellfun(@isempty,storedMetaData));
-bioProdRateData = cell(size(storedMetaData));
-experimentCount = length(dataIndex);
-
-% 3. load positions recorded on excel files
-x_junc = xlsread('xPositions_junction.xlsx');
-x_cells = xlsread('xPositions_cells.xlsx');
-
-
-% for all experiments (cells with data)
-for e = 1:experimentCount
-    
-    % 4. print experiment date
-    index = dataIndex(e);
-    date = storedMetaData{index}.date
-    
-    % 5. assign cell positions to 10-value array
-    x_cellPositions = x_cells(3:12,index)
-    
-    % 6. assign junc position and print
-    x_juncPosition = x_junc(3,index)
-    
-    % 7. store variables into copy of meta data
-    currentStructure = copyMD{index};
-    currentStructure.x_cell = x_cellPositions;
-    currentStructure.x_junc = x_juncPosition;
-    copyMD{index} = currentStructure;
-    
-end
-
-%% (15) continued
-% 8. if satisfied, save copy with new variable as true meta data
-storedMetaData = copyMD;
-save('storedMetaData.mat','storedMetaData')
