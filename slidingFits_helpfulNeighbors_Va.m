912a67371004aa1d196ca418cc619714cde224a7
Jen Nguyen <jknguyen@mit.edu>
2018-08-06 16:44:58 +0200

    mu caluclations for experiment 2018-06-15, with lower upper bound applied to fluc condition xys 1-10. this is a redo, as it got overwritten

--- slidingFits_helpfulNeighbors_Va.m
+++ slidingFits_helpfulNeighbors_Va.m
@@ -32,8 +32,10 @@
 %       12. repeat for all movies
 
 
-% last update: 2018 August 03
-% commit: mu caluclations for experiment 2018-08-01, with high upper bound in width applied
+% last update: 2018 August 06
+% commit: mu caluclations for experiment 2018-06-15, with lower upper bound
+%         in width applied to fluc positions 1-10. re-do, as it got
+%         overwritten previously.
 
 % OK lez go!
 
@@ -42,7 +44,7 @@
 % 0. initialize 
 clear
 clc
-experiment = '2018-08-01';
+experiment = '2018-06-15';
 
 % 0. open folder for experiment of interest
 newFolder = strcat('/Users/jen/Documents/StockerLab/Data/LB/',experiment);%,'  (t300)');
@@ -51,7 +53,8 @@ cd(newFolder);
 
 % 0. initialize trimmed track data
 
-load(strcat('lb-fluc-',experiment,'-width1p7-jiggle-0p5.mat'),'D','D5','T','rejectD');
+%load(strcat('lb-fluc-',experiment,'-width1p7-jiggle-0p5.mat'),'D','D5','T','rejectD');
+load(strcat('lb-fluc-',experiment,'-c123-width1p4-c4-1p7-jiggle-0p5.mat'),'D','D5','T','rejectD');
 numMovies = length(D5);
 
 
@@ -225,7 +228,7 @@ for n = 1:length(D5)
 end
 
 
-save(strcat('lb-fluc-',experiment,'-window5-width1p7-jiggle-0p5.mat'), 'D','D5','M', 'M_va', 'T','rejectD') %'D'
+save(strcat('lb-fluc-',experiment,'-window5-width1p4-1p7-jiggle-0p5.mat'), 'D','D5','M', 'M_va', 'T','rejectD') %'D'
 
 
 %% checks
