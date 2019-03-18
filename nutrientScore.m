f0de8cdcea58d715eedfd4329c0934acb035f090
Jen Nguyen <jknguyen@mit.edu>
2019-03-18 09:05:38 +0100

    edit parameter acquisition and allow for tracks with curve IDs that don't start with 1

--- nutrientScore.m
+++ nutrientScore.m
@@ -1,118 +1,8 @@
 % nutrientScore
 
 
-% goal: devise a quantitative measure of a cell's nutrient experience
-
-% for strategies, see comments preceding each version below
-
-% last update: jen, 2018 Apr 6
-
-% commit: correct column number for condition values. previously was trackNum
-
-%% version one
-% % convert timestamps to nutrient score, lower resolution especially at
-% % faster timescales
-
-% strategy:
-
-%       0.  initialize timestamp and curve data
-%       1.  split timestamps into quarters
-%       2.  from corrected timestamps, generate a binary nutrient signal, where: 1 = high and 0 = low           
-%       3.  calculate nScore for each curve in each track
-%               4.  for each track, isolate curve and binary nutrient signal
-%               5.  determine whether track contains any full curves
-%               5i. if not, score of track = NaN
-%               5ii. else, if track contains at least one full curve
-%                       6.  for each curve, isolate nutrient signal
-%                       7.  calculate score = sum(signal)/length(signal)
-%                       8.  save score
-%               8.  concatenate scores for all tracks
-%       9.  output binaryNutrientSignal and nScore vectors for entire condition
-% 
-
-
-% function [binaryNutrientSignal, nScore] = nutrientScore(timescale,conditionData)
-% 
-% 
-% % 0. initialize timestamp and volume data
-% correctedTime = conditionData(:,30);           % col 30  = timestamps, reflecing true time for all conditions in sec
-% curveID = conditionData(:,6);                  % col 6  =  curve ID per track
-% trackNum = conditionData(:,27);                % col 27 =  track number, not ID from particle tracking
-% condVals = conditionData(:,28);                % col 28 =  condition number
-% 
-% 
-% % 0. check that conditionData contains only one condition
-% %    if more, report error
-% if length(unique(condVals)) > 1
-%     
-%     error('ERROR: nutrientSource function requires that conditionData contain only ONE condition')
-%     % single condition is a requirement, because downstream, nScore
-%     % calculations and assignments require that track numbers NOT be repeated
-%     
-% end
-% 
-% 
-% % 1. split timestamps into quarters
-% timeInPeriods = correctedTime/timescale; % unit = sec/sec
-% timeInPeriodFraction = timeInPeriods - floor(timeInPeriods);
-% timeInQuarters = ceil(timeInPeriodFraction * 4);
-% 
-% 
-% % 2. from corrected timestamps, generate a binary nutrient signal where, 1 = high and 0 = low
-% binaryNutrientSignal = zeros(length(timeInQuarters),1);
-% binaryNutrientSignal(timeInQuarters == 1) = 1;
-% binaryNutrientSignal(timeInQuarters == 4) = 1;
-% 
-% 
-% % 3. calculate nScore for each curve in each track
-% 
-% nScore = [];
-% for tr = 10%1:max(trackNum)
-%     
-%     % 4. for each track, isolate curve and binary nutrient signal
-%     currentTrack_signal = binaryNutrientSignal(trackNum == tr);
-%     currentTrack_curves = curveID(trackNum == tr);
-%     
-%     % 5. determine whether track contains any full curves
-%     if sum(currentTrack_curves) == 0
-%         
-%         % if no full curves, score for track = NaN
-%         trackScore = NaN(length(currentTrack_signal),1);
-%         
-%     else
-%         
-%         % 6. for each curve,
-%         trackScore = NaN(length(currentTrack_signal),1);
-%         
-%         for cc = 1:max(currentTrack_curves)
-%             
-%             % 6. isolate nutrient signal
-%             currentCurve_curve = currentTrack_curves(currentTrack_curves == cc)
-%             currentCurve_signal = currentTrack_signal(currentTrack_curves == cc);
-%             
-%             % 7. calculate score
-%             score = mean(currentCurve_signal);
-%             
-%             % 8. save score
-%             trackScore(currentTrack_curves == cc) = score;
-%             
-%         end
-%         
-%     end
-%     
-%     % 8. concatenate scores for all tracks
-%     nScore = [nScore; trackScore];
-%     
-% end
-% 
-% 
-% % 9. output binaryNutrientSignal and nScore vectors for entire condition
-% 
-% end % end nutrientScore function
-
-%% version two
-% use timestamps of birth and division to calculate higher resolution
-% nutrient score
+% goal: devise a quantitative measure of a cell's nutrient experience,
+%       using timestamps of birth and division to calculate a nutrient score
 
 % strategy:
 
@@ -131,14 +21,22 @@
 %              11. concatenate scores for all tracks
 %      12. output binaryNutrientSignal and nScore vectors for entire condition
 
-function [binaryNutrientSignal, nScore] = nutrientScore(timescale,conditionData)
+
+% last update: jen, 2019 March 12
+% commit: edit parameter acquisition and allow for tracks with curve IDs that don't start with 1
+
+% ok let's go!
+
+%% 
+
+function [binaryNutrientSignal, nScore] = nutrientScore(timescale,dm)
 
 
 % 0. initialize timestamp and volume data
-correctedTime = conditionData(:,25);           % col 25  = timestamps, reflecing true time for all conditions in sec
-curveID = conditionData(:,6);                  % col 6  =  curve ID per track
-trackNum = conditionData(:,22);                % col 22 =  track number, not ID from particle tracking
-condVals = conditionData(:,23);                % col 23 =  condition number
+correctedTime = getGrowthParameter(dm,'correctedTime');    % timestamp in sec, corrected for lag in signal between junc and cell xy
+curveID = getGrowthParameter(dm,'curveFinder');            % curve ID of each individual cell cycle
+trackNum = getGrowthParameter(dm,'trackNum');              % track number, not ID from particle tracking
+condVals = getGrowthParameter(dm,'condition');                                       % condition number (1 = fluc, etc)
 
 
 % 0. check that conditionData contains only one condition
@@ -154,11 +52,13 @@ end
 
 % 1. calculate nScore for each track
 nScore = [];
+binaryNutrientSignal = [];
 
 for tr = 1:max(trackNum)
        
     % 2. isolate full curves and corresponding timestamps (lag corrected)
-    currentTrack_curves = curveID(trackNum == tr);
+    currentTrack_curveID = curveID(trackNum == tr);
+    currentTrack_curves = currentTrack_curveID(currentTrack_curveID > 0);
     currentTrack_times = correctedTime(trackNum == tr);
     
     % 3. determine whether track contains any full curves
@@ -166,41 +66,55 @@ for tr = 1:max(trackNum)
         
         % 4. if no full curves, score for track = NaN
         trackScore = NaN(length(currentTrack_curves),1);
+        trackBNS = NaN(length(currentTrack_curves),1);
         
     else
         
         % 5. if full curves are present, for each curve
-        trackScore = NaN(length(currentTrack_curves),1);
-        for cc = 1:max(currentTrack_curves)
+        trackScore = NaN(length(currentTrack_curveID),1);
+        trackBNS = NaN(length(currentTrack_curves),1);
+        for cc = min(currentTrack_curves):max(currentTrack_curves)
             
             % 6. determine time of birth and time of division
-            currentCurve_indeces = find(currentTrack_curves == cc);
+            currentCurve_indeces = find(currentTrack_curveID == cc);
             time_ofBirth = currentTrack_times(min(currentCurve_indeces));
             time_ofDivision = currentTrack_times(max(currentCurve_indeces));
          
             % 7. generate an artificially resolved time vector between birth and division
             resolved_time = linspace(time_ofBirth,time_ofDivision,100000)'; % in min
-            timeInPeriods = resolved_time/timescale; % unit = sec/sec
-            timeInPeriodFraction = timeInPeriods - floor(timeInPeriods);
-            timeInQuarters = ceil(timeInPeriodFraction * 4);
+            resolved_timeInPeriods = resolved_time/timescale; % unit = sec/sec
+            resolved_timeInPeriodFraction = resolved_timeInPeriods - floor(resolved_timeInPeriods);
+            resolved_timeInQuarters = ceil(resolved_timeInPeriodFraction * 4);
             
             % 8. from corrected timestamps, generate a binary nutrient signal where, 1 = high and 0 = low
-            binaryNutrientSignal = zeros(length(timeInQuarters),1);
-            binaryNutrientSignal(timeInQuarters == 1) = 1;
-            binaryNutrientSignal(timeInQuarters == 4) = 1;
+            resolvedBNS = zeros(length(resolved_timeInQuarters),1);
+            resolvedBNS(resolved_timeInQuarters == 1) = 1;
+            resolvedBNS(resolved_timeInQuarters == 4) = 1;
             
             % 9. calculate score
-            score = mean(binaryNutrientSignal);
+            score = mean(resolvedBNS);
             
             % 10. save score
             trackScore(currentCurve_indeces) = score;
+            
+            % 11. save binary signal
+            time_ofCurve = currentTrack_times(currentCurve_indeces);
+            timeInPeriods = time_ofCurve/timescale; % unit = sec/sec
+            timeInPeriodFraction = timeInPeriods - floor(timeInPeriods);
+            timeInQuarters = ceil(timeInPeriodFraction * 4);
+            
+            curveBNS = zeros(length(timeInQuarters),1);
+            curveBNS(timeInQuarters == 1) = 1;
+            curveBNS(timeInQuarters == 4) = 1;
+            trackBNS(currentCurve_indeces) = curveBNS;
+            
             
         end
         
     end
     % 11. concatenate scores for all tracks
     nScore = [nScore; trackScore];
-    
+    binaryNutrientSignal = [binaryNutrientSignal; trackBNS];
 end
 
 % 12. output binaryNutrientSignal and nScore vectors for entire condition
