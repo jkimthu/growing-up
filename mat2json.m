119655b8864709b5b3a7061ffa86ca77e3d1b10c
Jen Nguyen <jknguyen@mit.edu>
2019-11-27 18:51:06 +0100

    moved to sixpack-abs

--- mat2json.m
+++
@@ -1,5 +0,0 @@
-% conversion of data into JSON
-
-f = {'xy1','xy2','xy3','xy4','xy5','xy6','xy7','xy8','xy9','xy10','xy11','xy12','xy13','xy14','xy15','xy16','xy17','xy18','xy19','xy20','xy21','xy22','xy23','xy24','xy25','xy26','xy27','xy28','xy29','xy30','xy31','xy32','xy33','xy34','xy35','xy36','xy37','xy38','xy39','xy40'};
- 
-cell2struct(D6,f,40); 
